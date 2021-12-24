0bf0c69e18fe0c9f57d641f514432e6d024a4a4a
Ian Henderson <ianhenderson@protonmail.com>
2021-12-24 13:04:51 -0500

    initial commit

---
+++ 2-language-foundations/numbers/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("\n2.3.1 Integers and decimal (floating-point) numbers");
+    let twenty = 20;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
+    let addition = twenty + twenty_one + twenty_two;
+    println!(
+        "{} + {} + {} = {}",
+        twenty, twenty_one, twenty_two, addition
+    );
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+    let forty_twos = [42.0, 42f32, 42.0_f32];
+    println!("{:02}", forty_twos[0]);
+
+    println!("\n2.3.1 Integers and decimal (floating-point) numbers");
+    let three = 0b11;
+    let thirty = 0o36;
+    let three_hundred = 0x12C;
+    println!("base 10: {} {} {}", three, thirty, three_hundred);
+    println!("base 2:  {:b} {:b} {:b}", three, thirty, three_hundred);
+    println!("base 8:  {:o} {:o} {:o}", three, thirty, three_hundred);
+    println!("base 16: {:x} {:x} {:x}", three, thirty, three_hundred);
+}
